f90f6300647cfd97ab29957b750b653d1754e69d
Peter Gazdik <peter.gazdik@gmail.com>
2024-02-09 17:56:15 +0100

    fixing parameter propagation in dr-debug.bat

--- devrock-sdk/res/devrock-sdk/tools/jinni/bin/dr-debug.bat
+++ devrock-sdk/res/devrock-sdk/tools/jinni/bin/dr-debug.bat
@@ -2,5 +2,5 @@
 setlocal
 set ANT_OPTS=-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=y
 set GRADLE_DEBUG_OPTS=-Dorg.gradle.debug=true --no-daemon
-CALL dr
+CALL dr %*
 endlocal
