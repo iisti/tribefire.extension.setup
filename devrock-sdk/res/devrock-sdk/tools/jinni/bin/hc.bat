3ef97fd643a525f88b2fe4f6933af5d387e45df7
tryptamic <dirk.scheffler@modularmind.eu>
2024-04-11 09:22:22 +0200

    changed build kind detection

--- devrock-sdk/res/devrock-sdk/tools/jinni/bin/hc.bat
+++ devrock-sdk/res/devrock-sdk/tools/jinni/bin/hc.bat
@@ -4,12 +4,12 @@ setlocal EnableDelayedExpansion
 set HC_BUILD=%~dp0\hc-build
 
 REM differentiate between gradle and ant build scripts to call according build system
-if exist build.xml (
-    REM artifact build
-    CALL %HC_BUILD% run-ant %*
-) else (
+if exist parent\pom.xml (
     REM group build
     CALL %HC_BUILD% build %*
+) else (
+    REM artifact build
+    CALL %HC_BUILD% run-ant %*
 )
 
 endlocal
