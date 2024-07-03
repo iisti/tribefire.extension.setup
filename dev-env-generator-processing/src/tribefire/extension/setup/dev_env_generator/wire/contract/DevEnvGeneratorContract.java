05f84bd03b9bb795b0569bda46ab377df6b2d653
Peter Gazdik <peter.gazdik@gmail.com>
2024-07-03 12:32:45 +0200

    Fixing Apache License

--- dev-env-generator-processing/src/tribefire/extension/setup/dev_env_generator/wire/contract/DevEnvGeneratorContract.java
+++ dev-env-generator-processing/src/tribefire/extension/setup/dev_env_generator/wire/contract/DevEnvGeneratorContract.java
@@ -1,4 +1,6 @@
 // ============================================================================
+// Copyright BRAINTRIBE TECHNOLOGY GMBH, Austria, 2002-2022
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -10,17 +12,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// ============================================================================
-// ============================================================================
-// Copyright BRAINTRIBE TECHNOLOGY GMBH, Austria, 2002-2022
-// 
-// This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
-// 
-// This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
-// 
-// You should have received a copy of the GNU Lesser General Public License along with this library; See http://www.gnu.org/licenses/.
 // ============================================================================
 package tribefire.extension.setup.dev_env_generator.wire.contract;
 
