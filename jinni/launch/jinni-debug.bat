2f22126fe58e914e261bbc7071adeb335644b3f0
tryptamic <dirk.scheffler@modularmind.eu>
2024-07-22 20:06:56 +0200

    adapted launch scripts

--- jinni/launch/jinni-debug.bat
+++ jinni/launch/jinni-debug.bat
@@ -1,12 +1,5 @@
 @echo off
 setlocal
-
-if "%JAVA_HOME%"=="" (
-    set JAVA_EXECUTABLE=java    
-)else (
-    set JAVA_EXECUTABLE="%JAVA_HOME%\bin\java"
-)
-
-%JAVA_EXECUTABLE% -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=4000,suspend=y -Djinni.installationDir="%~dp0\..\\" -Dgm.ownCl=true -jar "%~dp0\..\lib\launch.jar" %*
-
+set JINNI_OPTS=-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=4000,suspend=y
+CALL %~dp0/jinni %*
 endlocal
--- jinni/launch/jinni.bat
+++ jinni/launch/jinni.bat
@@ -7,6 +7,22 @@ if "%JAVA_HOME%"=="" (
     set JAVA_EXECUTABLE="%JAVA_HOME%\bin\java"
 )
 
-%JAVA_EXECUTABLE% -Djinni.installationDir="%~dp0..\\" -Dgm.ownCl=true -jar "%~dp0\..\lib\launch.jar" %*
+set SHARED_LIB_PATH=%~dp0\..\..\shared-lib
+set PRIVATE_LIB_PATH=%~dp0\..\lib
+
+if exist "%PRIVATE_LIB_PATH%" (
+    set LIB_PATH=%PRIVATE_LIB_PATH%
+) else (
+    if exist "%SHARED_LIB_PATH%" (
+        set LIB_PATH=%SHARED_LIB_PATH%
+    ) else (
+        echo could not find any of: 
+        echo   %PRIVATE_LIB_PATH%
+        echo   %SHARED_LIB_PATH%
+        exit /b 1
+    )
+)
+
+%JAVA_EXECUTABLE% %JINNI_OPTS% -Djinni.installationDir="%~dp0..\\" -Dgm.ownCl=true -jar "%LIB_PATH%\launch.jar" %*
 
 endlocal
