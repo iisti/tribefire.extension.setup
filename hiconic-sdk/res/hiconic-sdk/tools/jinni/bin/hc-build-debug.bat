e4c0672372c8d68f77205a3ca544320747a03912
Peter Gazdik <peter.gazdik@gmail.com>
2024-07-30 18:40:26 +0200

    adding hc-build debug scripts to hiconic-sdk

---
+++ hiconic-sdk/res/hiconic-sdk/tools/jinni/bin/hc-build-debug.bat
@@ -0,0 +1,11 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+set TOOLS_DIR=%~dp0\..\..
+
+set DEVROCK_SDK_HOME=%TOOLS_DIR%\..
+set REPOSITORY_CONFIGURATION_DEVROCK_ANT_TASKS=%DEVROCK_SDK_HOME%\conf\repository-configuration-devrock.yaml
+
+CALL %TOOLS_DIR%\hc-build\bin\run-debug %*
+
+endlocal
